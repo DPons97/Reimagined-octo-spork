44b972ab02e6fd23515234e114efa4ae32dbb88d
luca collini <10533327@polimi.it>
2019-04-24 16:38:08 +0200

    changed image path from relative to absolute, fixed nextImg and tuned mean value to detect motion added frames

--- Executables/bkgndSubtraction.cpp
+++ Executables/bkgndSubtraction.cpp
@@ -21,13 +21,13 @@
 #include <opencv2/video.hpp>
 #include <time.h>
 
-#define FRAME_NAME "resources/frames/frame"
+#define FRAME_NAME "/home/pitohacapito/Documents/pii/Executables/resources/frames/frame"
 
 using namespace  cv;
 using namespace std;
 
 int sockfd;
-int currFrame = 1;
+long int currFrame = 1;
 clock_t lastTime = 0;
 Logger * mylog;
 
@@ -55,19 +55,26 @@ int main(int argc, char *argv[]) {
 
     Mat frame, fgMask;
     Scalar value;
+    int count = 0;
     while (true) {
-        frame = imread(nextImg().c_str(), CV_LOAD_IMAGE_COLOR);;
-        if (frame.empty())
+        frame = imread(nextImg().data(), CV_LOAD_IMAGE_COLOR);;
+        if (frame.empty()){
+            mylog->writeLog("ERROR OPENING FRAME");
             break;
+        }
 
         //update the background model
         pBackSub->apply(frame, fgMask);
 
         value = mean(fgMask);
-        if (value[0] > 80) {
+        mylog->writeLog(string("mean ").append(to_string(value[0])));
+
+        if (count > 3 && value[0] > 120) {
             mylog->writeLog(string("detected motion at frame ").append(to_string(currFrame)));
+            mylog->writeLog(string("number of elaborated frames: ").append(to_string(count)));
             break;
         }
+        count++;
     }
     sendImage(frame);
 
@@ -75,7 +82,16 @@ int main(int argc, char *argv[]) {
 }
 
 string nextImg(){
-    if(lastTime != 0)currFrame = currFrame + ((clock() - lastTime)/CLOCKS_PER_SEC)*30;
+    double ellapsedTime = ((double) (clock() - lastTime)/CLOCKS_PER_SEC );
+    //mylog->writeLog(string("ellapsed time: ").append(to_string(ellapsedTime)));
+    double toSleep = (0.3334-ellapsedTime);
+    //mylog->writeLog(string("toSleep: ").append(to_string(toSleep)));
+    if (toSleep > 0 ) {
+        usleep(toSleep * 1000000);
+        ellapsedTime = ((double) (clock() - lastTime)/CLOCKS_PER_SEC ) + toSleep;
+        //mylog->writeLog(string("ellapsed time: ").append(to_string(ellapsedTime)));
+    }
+    if(lastTime != 0) currFrame = currFrame + ellapsedTime * 30;
     lastTime = clock();
     string to_ret = string(FRAME_NAME).append(to_string(currFrame)).append(".jpg");
     mylog->writeLog(to_ret);
@@ -84,6 +100,7 @@ string nextImg(){
 
 void sendImage(Mat image){
     vector<uchar> buff;
+    char toLog[100];
 
     //alert image is coming
     write(sockfd, "imgsend",7);
@@ -91,17 +108,21 @@ void sendImage(Mat image){
     // send width
     char msg[5];
     sprintf(msg, "%d", image.cols);
-    printf("Sending cols: %s\n", msg);
+    sprintf(toLog, "Sending cols: %s\n", msg);
+    mylog->writeLog(toLog);
     write(sockfd, msg, 5);
 
     // send height
     sprintf(msg, "%d", image.rows);
-    printf("Sending rows: %s\n", msg);
+    sprintf(toLog,"Sending rows: %s\n", msg);
+    mylog->writeLog(toLog);
     write(sockfd, msg, 5);
 
     // send image
     int  imgSize = image.total()*image.elemSize();
-    printf("Sending image of size: %d", imgSize);
+    sprintf(toLog,"Sending image of size: %d", imgSize);
+    mylog->writeLog(toLog);
+
     write(sockfd, image.data, imgSize);
 
     //namedWindow( "Display window", WINDOW_AUTOSIZE );// Create a window for display.
